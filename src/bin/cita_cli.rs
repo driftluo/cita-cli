62c5c3f05128ca39bdb94d2bc65afd4ec6a6f03f
Qian Linfeng <thewawar@gmail.com>
2018-05-12 13:41:19 +0800

    Add command line argument parser

--- src/bin/cita_cli.rs
+++ src/bin/cita_cli.rs
@@ -1,10 +1,21 @@
+extern crate clap;
+
 extern crate cita_cli;
 
 use cita_cli::{JsonRpcParams, Client, ParamsValue};
 
 
 fn main() {
-    let mut client = Client::new().unwrap().add_url("http://127.0.0.1:1337");
+    let matches = clap::App::new("CITA CLI")
+        .arg(clap::Arg::with_name("url")
+             .long("url")
+             .default_value("http://127.0.0.1:1337")
+             .takes_value(true)
+             .help("JSONRPC server URL"))
+        .get_matches();
+    let url = matches.value_of("url").unwrap();
+
+    let mut client = Client::new().unwrap().add_url(url);
     let mut params = JsonRpcParams::new();
     params.insert(String::from("method"), ParamsValue::String(String::from("cita_blockNumber")));
     client.send_requests("cita_blockNumber", params);
