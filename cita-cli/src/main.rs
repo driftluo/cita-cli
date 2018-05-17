c4e53b1b1e4665f534d891a1086499b1a3202669
Qian Linfeng <thewawar@gmail.com>
2018-05-17 15:18:09 +0800

    Remove dotenv_codegen dependency

--- cita-cli/src/main.rs
+++ cita-cli/src/main.rs
@@ -1,26 +1,37 @@
 extern crate dotenv;
-#[macro_use]
-extern crate dotenv_codegen;
 extern crate clap;
 
 extern crate cita_tool;
+
+use std::env;
+use std::collections::HashMap;
 
 use dotenv::dotenv;
 
 use cita_tool::{Client, JsonRpcParams, ParamsValue};
 
+
+const ENV_JSONRPC_URL: &'static str = "JSONRPC_URL";
+const DEFAULT_JSONRPC_URL: &'static str = "http://127.0.0.1:1337";
+
 fn main() {
     dotenv().ok();
 
-    let jsonrpc_url = dotenv!("JSONRPC_URL");
+    let mut env_map = HashMap::new();
+    for (key, value) in env::vars() {
+        env_map.insert(key, value);
+    }
+    let default_jsonrpc_url = env_map
+        .remove(ENV_JSONRPC_URL)
+        .unwrap_or(DEFAULT_JSONRPC_URL.to_owned());
 
     let matches = clap::App::new("CITA CLI")
         .arg(
             clap::Arg::with_name("url")
                 .long("url")
-                .default_value(jsonrpc_url)
+                .default_value(default_jsonrpc_url.as_str())
                 .takes_value(true)
-                .help("JSONRPC server URL"),
+                .help(format!("JSONRPC server URL (dotenv: {})", ENV_JSONRPC_URL).as_str()),
         )
         .get_matches();
     let url = matches.value_of("url").unwrap();
