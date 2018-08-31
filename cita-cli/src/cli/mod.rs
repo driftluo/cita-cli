cb818faaaa0cacd29a097a81dc9baea4960cca06
Qian Linfeng <thewawar@gmail.com>
2018-09-01 00:50:02 +0800

    Add rustyline based interactive

--- cita-cli/src/cli/mod.rs
+++ cita-cli/src/cli/mod.rs
@@ -69,6 +69,11 @@ pub fn build_cli(default_url: &str) -> App {
                 .global(true)
                 .help("Display request parameters"),
         )
+        .arg(
+            Arg::with_name("rustyline")
+                .long("rustyline")
+                .help("Use rustyline readline library"),
+        )
 }
 
 /// Interactive parser
